4d311566f8af1889c5a3913f6fd847338ce89bec
kevholditch <kevholditch@hotmail.com>
2020-02-04 15:23:48 +0000

    custom settings

---
+++ screen1920.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr --output DP-0 --mode 1920x1080 --rotate right --pos 0x0
+xrandr --output DP-2 --primary --mode 1920x1080 --pos 1080x670 --rotate normal
+xrandr --output DP-6 --mode 1920x1080 --pos 3000x0 --rotate left
