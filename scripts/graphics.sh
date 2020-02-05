94be08475a0291f6e5365519bd9abf14ec1b1067
kevholditch <kevholditch@hotmail.com>
2020-02-05 09:38:56 +0000

    adding scripts

--- scripts/graphics.sh
+++ scripts/graphics.sh
@@ -2,5 +2,10 @@
 m=`nvidia-smi | sed -n '9p' | cut -d ' ' -f 20`
 n=`nvidia-smi | sed -n '9p' | cut -d ' ' -f 23`
 p=`nvidia-smi | sed -n '9p' | cut -d ' ' -f 29`
+
+if [[ -z "${p// }" ]]; then
+   p=`nvidia-smi | sed -n '9p' | cut -d ' ' -f 30`
+fi
+
 echo "$m/$n GPU:$p"
 
