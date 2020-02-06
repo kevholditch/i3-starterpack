8f544138e046676088684552cd20bc892a784190
kevholditch <kevholditch@hotmail.com>
2020-02-06 08:35:04 +0000

    adding scripts

---
+++ scripts/screen2560.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr --output DP-0 --mode 2560x1440 --rotate right --pos 0x0
+xrandr --output DP-2 --primary --mode 2560x1440 --pos 1440x670 --rotate normal
+xrandr --output DP-6 --mode 2560x1440 --pos 4000x0 --rotate left
